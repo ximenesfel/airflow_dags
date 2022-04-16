1129791aabb3f08ea8fd305559574c59840bd096
Felipe Ximenes <ximenesfel@gmail.com>
2022-04-16 18:50:19 -0300

    Include first dag

---
+++ dags/sample.py
@@ -0,0 +1,47 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.models import DagRun
+from airflow.utils.state import State
+from airflow.utils.dates import days_ago
+from kubernetes.client import models as k8s
+
+default_args = {
+    'owner': 'Airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(0),
+    'catchup': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'kuberntes_yaml',
+    default_args=default_args,
+    schedule_interval=None,
+    max_active_runs=1,
+    concurrency=10,
+)
+
+start = BashOperator(
+    task_id='start',
+    bash_command='echo 1',
+    dag=dag
+)
+
+training = KubernetesPodOperator(pod_template_file="./sample_pod.yaml",
+                                 dag=dag
+)
+
+finish = BashOperator(
+    task_id='finish',
+    bash_command='echo 1',
+    dag=dag
+)
+
+start >> training >> finish
