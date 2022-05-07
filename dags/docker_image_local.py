4ca8af3fb6e6149f7d9bc87e9a98d09033c8fb74
Felipe Ximenes Viana <felipe.viana@radixeng.com.br>
2022-05-07 17:42:37 -0300

    Add new dag

---
+++ dags/docker_image_local.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.configuration import conf
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+namespace = conf.get('kubernetes', 'NAMESPACE')
+
+# This will detect the default namespace locally and read the
+# environment namespace when deployed to Astronomer.
+if namespace =='default':
+    config_file = '/usr/local/airflow/include/.kube/config'
+    in_cluster = False
+else:
+    in_cluster = True
+    config_file = None
+
+dag = DAG('example_kubernetes_pod', schedule_interval=None, default_args=default_args)
+
+
+with dag:
+    KubernetesPodOperator(
+        namespace=namespace,
+        image="hello-world",
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        task_id="task-one",
+        in_cluster=in_cluster,  # if set to true, will look in the cluster, if false, looks for file
+        cluster_context="docker-desktop",  # is ignored when in_cluster is set to True
+        config_file=config_file,
+        is_delete_operator_pod=True,
+        get_logs=True,
+    )
